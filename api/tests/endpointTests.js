b694b71c521375546d8789c26ac3fbf849b43cef
Kerry Convery <kerry.convery@myob.com>
2017-06-09 12:37:26 +1200

    Added endpoint tests

---
+++ api/tests/endpointTests.js
@@ -0,0 +1,20 @@
+var chai = require('chai');
+
+describe("Test endpoints", () => {
+	
+	it("Test valid payslip returned when valid employee csv data supplied", () => {
+		
+	});
+
+	it("Test valid payslip returned when valid employee json data supplied", () => {
+		
+	});
+	
+	it("Test 400 error returned with validation error when invalid employee csv data supplied", () => {
+		
+	});	
+	
+	it("Test 400 error returned with validation error when invalid employee json data supplied", () => {
+		
+	});	
+});
