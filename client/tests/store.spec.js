69fcae9605784d58534200abcb0cdd13e25fe7f9
Kerry Convery <kerry.convery@myob.com>
2017-06-12 14:45:12 +1200

    test unit test

--- client/tests/store.spec.js
+++ client/tests/store.spec.js
@@ -1,9 +1,11 @@
-var chai = require('chai')
+import {expect} from 'chai'
+import {createStore} from '../store'
+
 
 describe("Store Tests.", () => {
 	
 	it("Check the payslip store", () => {
 		
-		
+		expect(true).equals(true)
 	})
 });
