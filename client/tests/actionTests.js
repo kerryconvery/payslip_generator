0cbc41441c67329f5428345c6d3dba0276a0f127
DESKTOP-UE92379\Kerry <kerry.convery@gmail.com>
2017-06-12 23:07:33 +1200

    more tests

--- client/tests/actionTests.js
+++ client/tests/actionTests.js
@@ -1,12 +1,29 @@
 import {expect} from 'chai'
+import nock from 'nock'
 import {generatePayslips} from '../actions/formActions';
 
 describe("Action Tests.", () => {
 	
-	it("Check generate payslip action type", () => {
-		
+	afterEach(() => {
+		nock.cleanAll()
+	})
+	
+	it("Check generate payslip action type", (done) => {
+		nock('http://localhost:3000')
+			.post('/payslips')
+			.reply(200, {body: ["response data"]})
+			
 		let action = generatePayslips("test data");
-				
-		expect(action.type).equals('GENERATE_PAYSLIPS');
+		
+		action.payload.then((payload) => {
+			
+			expect(action.type).equals('GENERATE_PAYSLIPS');
+			expect(payload.config.data).equals('test data');
+			expect(payload.config.url).equals('http://localhost:3000/payslips');
+			expect(payload.config.headers).to.deep.include({'Content-Type': 'application/csv'});
+			expect(payload.data.body[0]).equals("response data");
+			
+			done();
+		});
 	})
 });
